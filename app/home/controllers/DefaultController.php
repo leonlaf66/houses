0d9aca586e8abdf22068bb8e1e58bc8ee56fba0c
LiChunHua <lichunhua@medatc.com>
2017-07-21 18:26:36 +0800

    新增area选择

--- app/home/controllers/DefaultController.php
+++ app/home/controllers/DefaultController.php
@@ -5,13 +5,10 @@ class DefaultController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        /*
-        $rets = \common\estate\Rets::findOne('71779532');
-        $priceValueHtml = $rets->render()->get('market_time_property');
-
-        var_dump($priceValueHtml);
-        exit;
-        */
+        if (! \WS::$app->stateCode || isset($_GET['area'])) {
+            $this->layout = '@module/page/views/layouts/simple.phtml';
+            return $this->render('area.phtml');
+        }
 
         return $this->render('index.phtml');
     }
