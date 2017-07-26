9b9ebf76255747256377a3713a0cf123fc091daa
LiChunHua <lichunhua@medatc.com>
2017-07-26 11:33:17 +0800

    x

--- app/estate/controllers/AutocompleteController.php
+++ app/estate/controllers/AutocompleteController.php
@@ -2,38 +2,43 @@
 namespace module\estate\controllers;
 
 use WS;
-use yii\web\Controller;
+use module\core\Controller;
 
 class AutocompleteController extends Controller
 {
     public function actionIndex()
     {
-        $result = [];
+        $resultItems = [];
 
-        $cities = $this->_getEtcData('cities');
-        foreach($cities as $key=>$value) {
-            $result[] = [
-                'name'=>$value, 
-                'type'=>'City'
+        $towns = \common\catalog\Town::find()->where([
+            'state'=>\WS::$app->stateId
+        ])->all();
+
+        foreach ($towns as $town) {
+            $resultItems[] = [
+                'title' => $town->name,
+                'desc' => $town->name_cn.',MA'
+            ];
+            
+            if ($town->name_cn) {
+                $resultItems[] = [
+                    'title' => $town->name_cn,
+                    'desc' => $town->name.',MA'
+                ];
+            }
+        }
+
+        $zipcodes = \common\catalog\Zipcode::find()->where([
+            'state'=>\WS::$app->stateId
+        ])->all();
+
+        foreach ($zipcodes as $zipcode) {
+            $resultItems[] = [
+                'title' => $zipcode->zip,
+                'desc' => $zipcode->city_name.','.$zipcode->city_name_cn.',MA'
             ];
         }
 
-        $dictData = $this->_getEtcData('zip');
-        foreach($dictData['zip_code'] as $zip=>$options) {
-            $result[] = [
-                'name'=>''.$zip, 
-                'type'=>$options['city'].' [Zip Code]'
-            ];
-        }
-
-        echo json_encode([
-                'cities'=>$result
-            ]
-        );
-    }
-
-    protected function _getEtcData($name)
-    {
-        return include(COMMON_ROOT.'/estate/config/dicts/'.$name.'.php');
+        return $this->ajaxJson($resultItems);
     }
 }
