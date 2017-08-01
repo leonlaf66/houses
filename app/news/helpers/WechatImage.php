8686bb7fa30798d1c62d835016efdbff8efb315e
LiChunHua <lichunhua@medatc.com>
2017-08-01 16:43:37 +0800

    x

---
+++ app/news/helpers/WechatImage.php
@@ -0,0 +1,15 @@
+<?php
+namespace module\news\helpers;
+
+class WechatImage
+{
+    public static function parseContent($content)
+    {
+        return '2222';
+    }
+
+    public static function parseImageUrl($imgUrl)
+    {
+        return '2222';
+    }
+}
