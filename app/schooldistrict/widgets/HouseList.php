94fc0e7eaea3093059863af6a1a558e4465e9d20
LiChunHua <lichunhua@medatc.com>
2017-08-02 11:15:48 +0800

    x

---
+++ app/schooldistrict/widgets/HouseList.php
@@ -0,0 +1,25 @@
+<?php
+namespace module\schooldistrict\widgets;
+
+use WS;
+
+class HouseList extends \yii\base\Widget
+{
+    public $schoolDistrict = null;
+
+    public function run()
+    {
+        $towns = explode('/', $this->schoolDistrict->code);
+
+        $items = \common\estate\RetsIndex::find()
+            ->where(['in', 'town', $towns])
+            ->andWhere(['=', 'prop_type', 'SF'])
+            ->andWhere(['>', 'list_price', 700000])
+            ->andWhere(['is_show' => true])
+            ->orderBy(['id' => 'DESC'])
+            ->limit(6)
+            ->all();
+
+        return $this->render('house-list.phtml', ['items'=>$items]);
+    }
+}
