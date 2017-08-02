a05f7f95ed5602e964dbfc16d5605fe0f5b85bf1
LiChunHua <lichunhua@medatc.com>
2017-08-03 01:42:32 +0800

    x

--- app/estate/widgets/view/Roi.php
+++ app/estate/widgets/view/Roi.php
@@ -9,8 +9,27 @@ class Roi extends \yii\base\Widget
 
     public function run()
     {  
+        $listNo = $this->rets->list_no;
+        $zipCode = $this->rets->zip_code;
+
+        $result = WS::$app->db->createCommand('select * from rets_roi where "LIST_NO"=:id', [
+                ':id' => $listNo
+            ])->queryOne();
+        if (! $result) {
+            $result = [
+                'EST_ROI_CASH' => 0,
+                'EST_ANNUAL_INCOME_CASH' => 0
+            ];
+        }
+
+        $aveResult = WS::$app->db->createCommand('select * from rets_ave_roi where "ZIP_CODE"=:zip', [
+                ':zip' => $zipCode
+            ])->queryOne();
+
+        $result = array_merge($result, $aveResult);
+
         return $this->render('roi.phtml', [
-            'rets'=>$this->rets
+            'result'=>$result
         ]);
     }
 }
