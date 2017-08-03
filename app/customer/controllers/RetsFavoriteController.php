4e6e094fdb6e3da83a0db3f081bbee9652e59dc5
LiChunHua <lichunhua@medatc.com>
2017-08-03 18:29:14 +0800

    x

--- app/customer/controllers/RetsFavoriteController.php
+++ app/customer/controllers/RetsFavoriteController.php
@@ -5,12 +5,34 @@ use WS;
 
 class RetsFavoriteController extends \yii\web\Controller
 {
+    public function actionUpdate()
+    {
+        $result = -1;
+
+        if (! WS::$app->user->isGuest) {
+            $result = \common\customer\RetsFavorite::addOrRemove(WS::$app->request->get('list_no', 0), WS::$app->user->id);    
+        }
+
+        echo json_encode($result);
+    }
+
     public function actionAdd()
     {
         $result = -1;
 
         if (! WS::$app->user->isGuest) {
             $result = \common\customer\RetsFavorite::add(WS::$app->request->get('list_no', 0), WS::$app->user->id);    
+        }
+
+        echo json_encode($result);
+    }
+
+    public function actionRemove()
+    {
+        $result = -1;
+
+        if (! WS::$app->user->isGuest) {
+            $result = \common\customer\RetsFavorite::remove(WS::$app->request->get('list_no', 0), WS::$app->user->id);    
         }
 
         echo json_encode($result);
