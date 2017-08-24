a604a1e753407a37b8a399a8d90adf1859d33866
LiChunHua <lichunhua@medatc.com>
2017-08-24 09:34:51 +0800

    x

--- app/yellowpage/etc/config.php
+++ app/yellowpage/etc/config.php
@@ -2,7 +2,12 @@
 return [
 	'urlRules' => [
         'pro-service/'  => 'yellowpage/default/index',
+        'pro-service/search/city<city:\d+>/<type:\d+>/' => 'yellowpage/search/result',
+        'pro-service/search/city<city:\d+>/' => 'yellowpage/search/result',
+        
+        'pro-service/search/<type:\d+>/' => 'yellowpage/search/result',
         'pro-service/search/' => 'yellowpage/search/result',
+        
         'pro-service/<id:\d+>/' => 'yellowpage/default/view'
 	]
 ];
