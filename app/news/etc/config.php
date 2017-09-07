8b3d8ff86d8fb2c2bdaef57973955839acea0d80
LiChunHua <lichunhua@medatc.com>
2017-09-07 11:00:18 +0800

    x

--- app/news/etc/config.php
+++ app/news/etc/config.php
@@ -2,6 +2,7 @@
 return [
     'urlRules' => [
         'news/channel<type:\d+>/'=>'news/default/index',
+        'news/p-<page:\d+>/'  => 'news/default/index',
         'news/'  => 'news/default/index',
         'news/<id:\d+>/'=>'news/default/view',
         'news/pdf/'  => 'news/default/pdf',
--- app/yellowpage/etc/config.php
+++ app/yellowpage/etc/config.php
@@ -5,9 +5,11 @@ return [
         'pro-service/search/city<city:\d+>/<type:\d+>/' => 'yellowpage/search/result',
         'pro-service/search/city<city:\d+>/' => 'yellowpage/search/result',
         
+        'pro-service/search/<type:\d+>/<page:\d+>/' => 'yellowpage/search/result',
         'pro-service/search/<type:\d+>/' => 'yellowpage/search/result',
         'pro-service/search/' => 'yellowpage/search/result',
         
+        'pro-service/<id:\d+>/<page:\d+>/' => 'yellowpage/default/view',
         'pro-service/<id:\d+>/' => 'yellowpage/default/view'
 	]
 ];
