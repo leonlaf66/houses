994ee7e33be007e6c62ec333453847dd078974da
LiChunHua <lichunhua@medatc.com>
2017-09-07 21:43:05 +0800

    x

--- app/schooldistrict/widgets/Grid.php
+++ app/schooldistrict/widgets/Grid.php
@@ -18,14 +18,37 @@ class Grid extends \yii\base\Widget
             $items = [];
         }
 
+        $min = 5;
+        $maxState = ['value' => 0, 'idx' => 0];
+        $mTotal = 0;
+        foreach ($items as $idx => $item) {
+            $items[$idx]->width_value = floatval($item->value);
+
+            $value = floatval($item->value);
+            if ($value < $min) {
+                $items[$idx]->width_value = $min;
+                $mTotal += $min - $value;
+            } elseif ($value > $maxState['value']) {
+                $maxState['value'] = $value;
+                $maxState['idx'] = $idx;
+            }
+        }
+
+        if ($mTotal > 0) {
+            $items[$maxState['idx']]->width_value = $items[$maxState['idx']]->width_value - $mTotal;
+        }
+
         $colors = ['#fac68d', '#aee9be', '#a3c9fb', '#99bd2a', '#f66c6c'];
         $idx = 0;
-        return array_map(function ($item) use (& $idx, $colors) {
+        $items = array_map(function ($item) use (& $idx, $colors) {
+            $item->width_value = $item->width_value . '%';
             $item->height = $idx === 0 ? '24px' : '20px';
             $item->bgColor = $colors[$idx] ?? '#f66c6c';
             $idx ++;
             return $item;
         }, $items);
+
+        return $items;
     }
 
     public function k12($schools)
