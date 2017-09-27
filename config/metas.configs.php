ebd21f7f3c9852ba13c22f27c826aac3551339fd
LiChunHua <lichunhua@medatc.com>
2017-09-27 09:04:27 +0800

    x

---
+++ config/metas.configs.php
@@ -0,0 +1,55 @@
+<?php
+return [
+    'home/default/area' => [
+        'name' => '区域选择页',
+        'variables' => '%area%',
+    ],
+    'home/default/index' => [
+        'name' => '首页',
+        'variables' => '%area%',
+    ],
+    'estate/house/purchase' => [
+        'name' => '购房搜索页',
+        'variables' => '%area%',
+    ],
+    'estate/house/lease' => [
+        'name' => '租房搜索页',
+        'variables' => '%area%',
+    ],
+    'estate/house/purchase/view' => [
+        'name' => '购房房源详情页',
+        'variables' => '%name%',
+    ],
+    'estate/house/lease/view' => [
+        'name' => '租房房源详情页',
+        'variables' => '%name%',
+    ],
+    'schooldistrict/default/index' => [
+        'name' => '学区首页',
+        'variables' => '%area%',
+    ],
+    'schooldistrict/default/view' => [
+        'name' => '学区详情页',
+        'variables' => '%name%',
+    ],
+    'yellowpage/default/index' => [
+        'name' => '黄页首页',
+        'variables' => '%area%',
+    ],
+    'yellowpage/search/result' => [
+        'name' => '黄页列表页',
+        'variables' => '%area%',
+    ],
+    'yellowpage/default/view' => [
+        'name' => '黄页详情页',
+        'variables' => '%name%',
+    ],
+    'news/default/index' => [
+        'name' => '新闻首页',
+        'variables' => '%area%',
+    ],
+    'news/default/view' => [
+        'name' => '新闻详情页',
+        'variables' => '%area% %name%',
+    ],
+];
