2ac1c2277999dc4a513d2e761b0015dfe5cec0fe
LiChunHua <lichunhua@medatc.com>
2017-10-13 21:20:35 +0800

    x

--- app/estate/widgets/home/Top.php
+++ app/estate/widgets/home/Top.php
@@ -5,7 +5,27 @@ class Top extends \yii\base\Widget
 {
     public function run()
     {
-        $groups = \WS::getStaticData('home.rets.top');
+        $items = \models\SiteSetting::getJson('home.luxury.houses', true);
+        
+        /*分列 以便更容易渲染输出*/
+        $groups = [];
+        $groupIndex = 0;
+        $colTotal = 0;
+        foreach ($items as $item) {
+            list($colWidth, $cols) = explode('-', $item->col_rule);
+            $colWidth = intval($colWidth);
+            $cols = intval($cols);
+
+            if ($colTotal + $colWidth < $cols) { // 当前组内
+                $groups[$groupIndex][] = (array)$item;
+                $colTotal += $colWidth;
+            } else { // 到达组内最后一个
+                $groups[$groupIndex][] = (array)$item; // 实际是前一组的最后一个
+                $colTotal = 0;
+                $groupIndex ++;
+            }
+        }
+
         return $this->render('top.phtml', [
             'groups'=>$groups,
             'imageRoot'=>media_url('rets/home-top')
