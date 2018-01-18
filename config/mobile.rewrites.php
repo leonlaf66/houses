bf901f5754d8ee533a7ba2439de26b01b58f0e75
LiChunHua <lichunhua@medatc.com>
2018-01-18 17:16:31 +0800

    x

--- config/mobile.rewrites.php
+++ config/mobile.rewrites.php
@@ -4,21 +4,30 @@ return [
         return '/';
     },
     'estate/house/purchase' => function ($req) {
-        return '/purchase.html';
+        return '/house/search/purchase';
     },
     'estate/house/lease' => function ($req) {
-        return '/rental.html';
+        return '/house/search/lease';
     },
     'estate/house/purchase/view' => function ($req) {
-        return ['/home.html', 'id' => $req->get('id')];
+        return '/house/' . $req->get('id');
     },
     'estate/house/lease/view' => function ($req) {
-        return ['/home.html', 'id' => $req->get('id')];
+        return '/house/' . $req->get('id');
+    },
+    'yellowpage/default/index' => function ($req) {
+        return '/yellowpage';
+    },
+    'yellowpage/search/result' => function ($req) {
+        return '/yellowpage';
+    },
+    'yellowpage/default/view' => function ($req) {
+        return 'yellowpage/' . $req->get('type_id');
     },
     'news/default/index' => function ($req) {
-        return '/newsMore.html';
+        return '/news';
     },
     'news/default/view' => function ($req) {
-        return ['/news.html', 'id'=>$req->get('id')];
+        return '/news/' . $req->get('id');
     }
 ];
