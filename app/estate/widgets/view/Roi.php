fd151a02f18551fd5e59f620f027dec429bac759
LiChunHua <lichunhua@medatc.com>
2018-03-19 10:54:02 +0800

    x

--- app/estate/widgets/view/Roi.php
+++ app/estate/widgets/view/Roi.php
@@ -12,13 +12,14 @@ class Roi extends \yii\base\Widget
         $listNo = $this->rets->list_no;
         $zipCode = $this->rets->zip_code;
 
-        $result = WS::$app->db->createCommand('select * from house_info_roi where "LIST_NO"=:id', [
+        $result = WS::$app->mlsdb->createCommand('select est_roi * 100 as "EST_ROI_CASH", est_rental * 12 as "EST_ANNUAL_INCOME_CASH" from mls_rets where "list_no"=:id', [
                 ':id' => $listNo
             ])->queryOne();
+
         if (! $result) {
             $result = [
-                'EST_ROI_CASH' => 0,
-                'EST_ANNUAL_INCOME_CASH' => 0
+                'EST_ROI_CASH' => null,
+                'EST_ANNUAL_INCOME_CASH' => null
             ];
         }
 
@@ -27,9 +28,8 @@ class Roi extends \yii\base\Widget
             ])->queryOne();
         if (! $aveResult) {
             $aveResult = [
-                'AVE_ROI_CASH' => 0,
-                'EST_ANNUAL_INCOME_CASH' => 0,
-                'AVE_ANNUAL_INCOME_CASH' => 0
+                'AVE_ROI_CASH' => null,
+                'AVE_ANNUAL_INCOME_CASH' => null
             ];
         }
 
