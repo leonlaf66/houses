949645647e425d1ce37c77dd1c96048ba47de97f
LiChunHua <lichunhua@medatc.com>
2018-06-01 12:07:25 +0800

    x

--- app/cms/components/Page.php
+++ app/cms/components/Page.php
@@ -86,9 +86,9 @@ class Page extends \yii\base\Component
 
         static $metasCache = [
             'home/default/area' => [
-                'title' => ['Area Section', '区域选择'],
-                'keywords' => '',
-                'description' => ''
+                'title' => ['Home - Housing for sale', '首页 - 美国买房'],
+                'keywords' => '海外房产,海外购房,美国房产网,北美房产网,海外买房,美国置业,海外置业,海外投资,美国投资,美国炒房,海外炒房,美国房地产,美国房产,美国购房,美国买房,北美房地产,美国房产咨询,北美房产咨询,波士顿房产咨询,波士顿买房,波士顿租房,波士顿房产,波士顿购房,波士顿房产网,波士顿房产网,波士顿置业,波士顿投资,波士顿炒房,波士顿房地产',
+                'description' => '米乐居是美国房地产网络平台，本站为用户提供最新、最全的实时房源，提供国内用户一条龙房产服务。让您不再为海外置业选房、看房、买房、养房发愁。'
             ]
         ];
         if (! isset($metasCache[$this->id])) {
