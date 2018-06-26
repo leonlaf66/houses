856e803d6ee6d66a72c3fd4c7565d0da7e4c230c
LiChunHua <lichunhua@medatc.com>
2018-06-26 09:09:47 +0800

    x

--- app/page/widgets/FooterBefore.php
+++ app/page/widgets/FooterBefore.php
@@ -7,4 +7,15 @@ class FooterBefore extends \yii\base\Widget
     {
         return $this->render('html/footer-before.phtml');
     }
+
+    public function getAreas()
+    {
+      return [
+        'ma' => tt('Boston', '波士顿'),
+        'ny' => tt('New York', '纽约'),
+        'ga' => tt('Atlanta', '亚特兰大'),
+        'ca' => tt('Los Angel', '洛杉矶'),
+        'il' => tt('Chicago', '芝加哥')
+      ];
+    }
 }
